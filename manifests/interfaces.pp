b7ef59f1179da74aea7e893dfd2d890f73af68e6
estheban <et@etiennelachance.com>
2012-06-12 16:24:50 -0400

    remove temporary test

--- manifests/interfaces.pp
+++ manifests/interfaces.pp
@@ -2,14 +2,11 @@ class network::interfaces($interfaces={}, $mappings={}, $auto=[]) {
 
   file { "/etc/network/interfaces":
     content => template("network/interfaces.erb"),
-    notify  => Service["networking"],
   }
 
   service {"networking":
-	#ensure => running,
-	#enable => true,
-	hasrestart => true,
-	hasstatus => false,
-	subscribe => File["/etc/network/interfaces"],
+  	hasrestart => true,
+  	hasstatus => false,
+  	subscribe => File["/etc/network/interfaces"],
   }
 }
