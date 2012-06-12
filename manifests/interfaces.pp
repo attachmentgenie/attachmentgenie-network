8a51706c373386e0778899c94f7e3df0017a0b13
estheban <et@etiennelachance.com>
2012-06-12 16:00:09 -0400

    Add networking service

--- manifests/interfaces.pp
+++ manifests/interfaces.pp
@@ -2,5 +2,11 @@ class network::interfaces($interfaces={}, $mappings={}, $auto=[]) {
 
   file { "/etc/network/interfaces":
     content => template("network/interfaces.erb"),
+    notify  => Service["networking"],
+  }
+
+  service {"networking":
+	ensure => running,
+	enable => true
   }
 }
